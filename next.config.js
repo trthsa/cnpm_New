274ccf51e1b58b5a01b84920ea00c3cbb51ec311
TS <trthsan@gmail.com>
2022-12-19 21:18:16 +0700

    add

--- next.config.js
+++ next.config.js
@@ -2,7 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
- 
-}
+  images: {
+    remotePatterns: {
+      protocol: "https",
+      hostname: "**",
+    },
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
